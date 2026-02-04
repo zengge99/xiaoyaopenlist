a177525dd1f5435a414bf8cfb03c7ce758b1d5d5
zengge99 <83830807+zengge99@users.noreply.github.com>
2026-02-04 15:36:24 +0800

    ok

--- drivers/all.go
+++ drivers/all.go
@@ -81,6 +81,7 @@ import (
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/wopan"
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/wps"
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/yandex_disk"
+	_ "github.com/OpenListTeam/OpenList/v4/drivers/strm_list"
 )
 
 // All do nothing,just for import
